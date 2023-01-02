2318c9afb37c7b2575c298d9a1e8df7106d2e87c
DamienCoop <67594538+netwerkfix@users.noreply.github.com>
2023-01-02 09:50:04 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,9 +21,11 @@ set -v
 
 cd /etc/systemd/system/
 wget https://raw.githubusercontent.com/netwerkfix/Gns3-prep/378e4aa4ddcddf12c6cf1829003ccee08b5d8a5c/installer.service
+chmod 755 installer.service
 mkdir -p /tmp/gns-installer
 cd /tmp/gns-installer
 wget https://raw.githubusercontent.com/netwerkfix/Gns3-prep/main/remote-install.sh
+chmod 755 remote-install.sh
 
 sleep 5
 clear
